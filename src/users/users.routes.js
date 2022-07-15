99ed6d0a350e13bd9889e9b971b769b9072742ee
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-15 12:25:56 -0300

    feat: create route user create

--- src/users/users.routes.js
+++ src/users/users.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { findAllUsersController, } = require('./users.controller');
 
 router.get('/', findAllUsersController);
+router.post('/create', createUserController);
 
 
 module.exports = router;
