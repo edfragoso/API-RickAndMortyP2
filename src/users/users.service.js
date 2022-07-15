934a7a68c17ecf1df65af2fe06d59409bc5cddd9
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-15 11:25:53 -0300

    feat: create findUserByEmailService

--- src/users/users.service.js
+++ src/users/users.service.js
@@ -2,11 +2,13 @@ const User = require('./User');
 
 const findAllUsersService = async () => await User.find();
 
-const findUserByNameService = async (username) => await User.findOne({username: username});
+const findUserByNameService = async (username) => await User.findOne({ username: username });
 
+const findUserByEmailService = async (email) => await User.findOne({ email: email });
 
 
 module.exports = {
     findAllUsersService,
-    findUserByNameService
+    findUserByNameService,
+    findUserByEmailService,
 }
