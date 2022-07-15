fa0761e9345ae4b078e6769197a4d926dc98f089
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-15 00:35:27 -0300

    chore: add character and users

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const databaseConnection = require('./src/database/database');
+const characterRoutes = require('./src/characters/characters.routes');
+const userRoutes = require('./src/users/users.routes');
 
 
 require('dotenv').config();
@@ -12,6 +14,10 @@ databaseConnection();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/users', userRoutes);
+app.use('/characters', characterRoutes);
+
 
 
 app.listen(port, () => {
