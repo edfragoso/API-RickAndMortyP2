26630e23ab5d38092ae4cbfc1942f4381d09362a
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-15 11:13:58 -0300

    feat: add findUserByNameService

--- src/users/users.service.js
+++ src/users/users.service.js
@@ -2,8 +2,11 @@ const User = require('./User');
 
 const findAllUsersService = async () => await User.find();
 
+const findUserByNameService = async (username) => await User.findOne({username: username});
+
 
 
 module.exports = {
     findAllUsersService,
+    findUserByNameService
 }
