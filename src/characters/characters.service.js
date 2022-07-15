6df3b5f29ee48e911b28771d553ad09a2d7504eb
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-15 12:28:30 -0300

    feat: create findByIdCharaterService

--- src/characters/characters.service.js
+++ src/characters/characters.service.js
@@ -2,9 +2,10 @@ const Character = require('./Character');
 
 const findAllCharacterService = async () => await Character.find().populate('user');
 
-
+const findByIdCharacterService = async (id) => await Character.findById(id);
 
 
 module.exports = {
     findAllCharacterService,
+    findByIdCharacterService,
 }
