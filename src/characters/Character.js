4840d74235651209d849187803138344ec680c9e
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-14 23:33:55 -0300

    feat: config schema character

---
+++ src/characters/Character.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const CharacterSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    name: { type: String, required: true, unique: true },
+    imageUrl: { type: String, required: true },
+  },
+  { versionKey: false },
+);
+
+const Character = mongoose.model('Character', CharacterSchema, 'characters');
+
+module.exports = Character;
