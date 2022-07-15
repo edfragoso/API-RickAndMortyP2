e68f7ecbcaa68b0728fccd1941daacb3a8e72652
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-15 12:29:51 -0300

    feat: create route findByIdCharacter

--- src/characters/characters.routes.js
+++ src/characters/characters.routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const {
     findAllCharacterController,
+    findByIdCharacterController
 } = require('./characters.controller');
 
 
 router.get('/', findAllCharacterController);
+router.get('/find/:id', findByIdCharacterController);
 
 
 
