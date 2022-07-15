e285cd780d276c60bca401d79269a5ac6d77f72b
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-15 12:55:41 -0300

    feat: add auth controller

---
+++ src/auth/auth.controller.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcryptjs');
+const { loginService, generateToken } = require('./auth.service');
+
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await loginService(email);
+
+    if (!user) {
+      return res.status(400).send({ message: 'Invalid login info' });
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).send({ message: 'Invalid login info' });
+    }
+
+    const token = generateToken(user.id);
+
+    res.send({ token });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+module.exports = loginController;
