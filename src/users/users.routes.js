524d135833fd40907e2038b9374724c88fc0d558
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-15 12:58:26 -0300

    chore: add auth middleware

--- src/users/users.routes.js
+++ src/users/users.routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const authMiddleware = require('../auth/auth.middleware');
 const { findAllUsersController, createUserController} = require('./users.controller');
 
-router.get('/', findAllUsersController);
+router.get('/', authMiddleware, findAllUsersController);
 router.post('/create', createUserController);
 
 
