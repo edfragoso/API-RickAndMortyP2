76dfbc09d6baccb45d8ed55840e5589252907d1b
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-07-14 23:27:54 -0300

    chore: add connection mongoDB and config port

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const databaseConnection = require('./src/database/database');
 
 
 require('dotenv').config();
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
+databaseConnection();
 
 app.use(cors());
 app.use(express.json());
